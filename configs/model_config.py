e2efeedde6a6fcaa515629367c7152cbd33760d2
Simon <54770908+simonZhou86@users.noreply.github.com>
2024-04-11 19:10:33 -0400

    Update model_config.py

--- configs/model_config.py
+++ configs/model_config.py
@@ -10,8 +10,14 @@ n_codes = 512
 vae_lr = 0.0001
 dis_lr = 0.0001
 lr_decay = True
+codebook_legacy_loss_beta = 0.25
 dis_start = 2500
 perp_num = 3
+l1_weight: 4.0, # l1 loss weight
+perp_weight: 1.0, # perceptual loss weight
+vq_weight: 1.0, # vq loss weight
+gan_feat_weight: 4.0, # feature matching loss weight
+img_grad_weight: 4.0, # image gradient loss weight
 # save dirs
 trail = "vqgan"
 data_dir = "./datapath"
