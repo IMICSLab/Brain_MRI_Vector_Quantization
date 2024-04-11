bc0eb9b75b5bc34c37309b5cbace8ad410d8ff2c
Simon <54770908+simonZhou86@users.noreply.github.com>
2024-04-11 19:29:57 -0400

    Add files via upload

--- train_transformer.py
+++ train_transformer.py
@@ -11,70 +11,56 @@ from torchvision import utils as vutils
 import sys
 sys.path.append("../")
 
-import meta_config as c
+import meta_config as mc
 from utils import set_seed
 from utils.get_data import *
-from model.vqgan_transformer_lat8 import VQGANTransformer
+from model.vqgan_transformer import VQGANTransformer
 from utils.plot_loss import plot_loss
 from utils.save_gen_img import show
 
 set_seed.random_seed(0, True)
 
-data_dir = c.data_dir
+data_dir = mc.data_dir
 
-res_dir = c.res_dir_imgOnly
+res_dir = mc.save_dir
 
 print("loading data: ")
-Rois = torch.load(os.path.join(data_dir, "LGG_ROI_128_train.pt"))
+Rois = torch.load(os.path.join(data_dir, "data.pt"))
 print("finish loading.")
 
-BATCH_SIZE = 3
-z_dim = (8,8,8) #(4,4,4) # last conv dim, bottleneck dim, for decoder
-EPOCH = 1500
-TRAIL = "vqgan_v2_stage2V2"#"1_stage2V2" #3
+BATCH_SIZE = mc.bs
+z_dim = mc.z_dim #(4,4,4) # last conv dim, bottleneck dim, for decoder
+EPOCH = mc.epoch
+TRAIL = mc.trail
 
 if not os.path.exists(res_dir + "/trail{}".format(TRAIL)):
     os.makedirs(res_dir + "/trail{}".format(TRAIL))
 
-config_dict = {}
 
-#print("this version includes the codebook loss")
-config_dict["Description"] = "Stage 2 of the vqgan model, logits cut based on sos_token, Train transformer, latent dim 8x8x8=512"
-config_dict["Trail"] = TRAIL
-config_dict["Batch Size"] = BATCH_SIZE
-config_dict["z_dim"] = z_dim
-config_dict["Epoch"] = EPOCH
-#config_dict["lr decay"] = lr_decay
-
-
-# total = list(range(Rois.shape[0]))
-# val_ind = list(np.random.choice(total, size=1, replace=False))
-# target_valid = [i for i in total if i not in val_ind]
-target_valid = list(range(Rois.shape[0]))
+total = list(range(Rois.shape[0]))
+val_ind = list(np.random.choice(total, size=BATCH_SIZE, replace=False))
+target_valid = [i for i in total if i not in val_ind]
 # get dataset
 #dataset = get_data_loader(Imgs, Msks, pIDs, target_valid, shuffle = True, norm = False)
 dataset = get_data_loader_128(Rois, target_valid, shuffle = True, norm = False)
-#val_dataset = get_data_loader_128(Rois, val_ind, shuffle = False, norm = False)
+val_dataset = get_data_loader_128(Rois, val_ind, shuffle = False, norm = False)
 
-# target_valid = list(range(Rois.shape[0]))
-# get dataset
-#dataset = get_data_loader(Imgs, Msks, pIDs, target_valid, shuffle = True, norm = False)
-# dataset = get_data_loader_128(Rois, target_valid, shuffle = True, norm = False)
 
 train_loader = torch.utils.data.DataLoader(dataset=dataset,
                                             batch_size=BATCH_SIZE,
                                             shuffle=True)
 
-# val_loader = torch.utils.data.DataLoader(dataset=val_dataset,
-#                                             batch_size=1,
-#                                             shuffle=False)
+val_loader = torch.utils.data.DataLoader(dataset=val_dataset,
+                                            batch_size=BATCH_SIZE,
+                                            shuffle=False)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # autoencoder weights
-model_weights = "./trailvqgan_v2/pretrained_model/epoch_3999.pt"
-transformer_model = VQGANTransformer(z_dim, model_weights, device)
-
+model_weights = mc.ae_weights
+n_codes = mc.n_codes
+transformer_model = VQGANTransformer(z_dim, n_codes, model_weights, device)
+lr = mc.lr
 
 def configure_optimizers(model):
     decay, no_decay = set(), set()
@@ -103,7 +89,7 @@ def configure_optimizers(model):
         {"params": [param_dict[pn] for pn in sorted(list(no_decay))], "weight_decay": 0.0},
     ]
 
-    optimizer = torch.optim.AdamW(optim_groups, lr=4.5e-06, betas=(0.9, 0.95)) # 4.5e-06
+    optimizer = torch.optim.AdamW(optim_groups, lr=lr, betas=(0.9, 0.95)) # 4.5e-06
     return optimizer
 
 optim_transformer = configure_optimizers(transformer_model)
@@ -114,12 +100,6 @@ print("Starting Training Loop...")
 
 use_mixed_precision = True
 scaler = torch.cuda.amp.GradScaler(enabled=use_mixed_precision)
-
-with open(res_dir + "/trail{}/config.txt".format(TRAIL), 'w') as f: 
-    for key, value in config_dict.items():
-        f.write("{}:{}\n".format(key, value))
-f.close()
-
 
 ce_losses = []
 
@@ -176,38 +156,40 @@ for epoch in range(EPOCH):
     if (epoch+1) % 10 == 0:
         transformer_model.eval()
         with torch.no_grad():
-            rand_ind = np.random.randint(0, 3)
-            sample_data = data[rand_ind].unsqueeze(0)
-            _, rec_and_pred_img = transformer_model.log_images(sample_data)
-            rec_and_pred_img = rec_and_pred_img.detach().cpu()
-            # each image should be 1,1,128,128,128
-            orig = rec_and_pred_img[0].squeeze(0).squeeze(0)
-            rec = rec_and_pred_img[1].squeeze(0).squeeze(0)
-            # image generated with half indices masked
-            half_random = rec_and_pred_img[2].squeeze(0).squeeze(0)
-            # image generated with full indices masked (only provide 1)
-            full_random = rec_and_pred_img[3].squeeze(0).squeeze(0)
-            full_random_mostlike = rec_and_pred_img[4].squeeze(0).squeeze(0)
-            
-            assert orig.shape == rec.shape == half_random.shape == full_random.shape == full_random_mostlike.shape, "logging images have different shape!"
-            
-            rand_ind = np.array(list(range(50, 50+16))) #np.random.choice(orig.shape[0], size=16, replace=False)
+            for i, data_b in enumerate(val_loader):
+                data = get_data_batch_ROI_128(data_b, device)
+                rand_ind = np.random.randint(0, BATCH_SIZE)
+                sample_data = data[rand_ind].unsqueeze(0)
+                _, rec_and_pred_img = transformer_model.log_images(sample_data)
+                rec_and_pred_img = rec_and_pred_img.detach().cpu()
+                # each image should be 1,1,128,128,128
+                orig = rec_and_pred_img[0].squeeze(0).squeeze(0)
+                rec = rec_and_pred_img[1].squeeze(0).squeeze(0)
+                # image generated with half indices masked
+                half_random = rec_and_pred_img[2].squeeze(0).squeeze(0)
+                # image generated with full indices masked (only provide 1)
+                full_random = rec_and_pred_img[3].squeeze(0).squeeze(0)
+                full_random_mostlike = rec_and_pred_img[4].squeeze(0).squeeze(0)
+                
+                assert orig.shape == rec.shape == half_random.shape == full_random.shape == full_random_mostlike.shape, "logging images have different shape!"
+                
+                rand_ind = np.array(list(range(50, 50+16))) #np.random.choice(orig.shape[0], size=16, replace=False)
 
-            show(orig, rand_ind, res_dir, TRAIL, "orig_img_epoch{}_img.png".format(epoch), img_save_dir = "rec_img")
-            show(rec, rand_ind, res_dir, TRAIL, "rec_img_epoch{}_img.png".format(epoch), img_save_dir = "rec_img")
-            show(half_random, rand_ind, res_dir, TRAIL, "half_rand_img_epoch{}_img.png".format(epoch), img_save_dir = "rec_img")
-            show(full_random, rand_ind, res_dir, TRAIL, "generated_img_formDist_epoch{}_img.png".format(epoch), img_save_dir = "rec_img")
-            show(full_random_mostlike, rand_ind, res_dir, TRAIL, "generated_img_mostlike_epoch{}_img.png".format(epoch), img_save_dir = "rec_img")
+                show(orig, rand_ind, res_dir, TRAIL, "orig_img_epoch{}_img.png".format(epoch), img_save_dir = "rec_img")
+                show(rec, rand_ind, res_dir, TRAIL, "rec_img_epoch{}_img.png".format(epoch), img_save_dir = "rec_img")
+                show(half_random, rand_ind, res_dir, TRAIL, "half_rand_img_epoch{}_img.png".format(epoch), img_save_dir = "rec_img")
+                show(full_random, rand_ind, res_dir, TRAIL, "generated_img_formDist_epoch{}_img.png".format(epoch), img_save_dir = "rec_img")
+                show(full_random_mostlike, rand_ind, res_dir, TRAIL, "generated_img_mostlike_epoch{}_img.png".format(epoch), img_save_dir = "rec_img")
         
-        half_random = (half_random + 1) / 2
-        half_random = half_random.permute(1,2,0).contiguous()
-        print("half random range: ", torch.min(half_random), torch.max(half_random))
-        full_random = (full_random + 1) / 2
-        full_random = full_random.permute(1,2,0).contiguous()
-        print("full random range: ", torch.min(full_random), torch.max(full_random))
-        torch.save(half_random, res_dir + "/trail{}".format(TRAIL) + "/half_random_generates_epoch{}.pt".format(epoch))
-        torch.save(full_random, res_dir + "/trail{}".format(TRAIL) + "/full_random_generates_epoch{}.pt".format(epoch))
-        torch.save(transformer_model.state_dict(), res_dir + "/trail{}/pretrained_model/".format(TRAIL) + "epoch_{}.pt".format(epoch))
+        # half_random = (half_random + 1) / 2
+        # half_random = half_random.permute(1,2,0).contiguous()
+        # print("half random range: ", torch.min(half_random), torch.max(half_random))
+        # full_random = (full_random + 1) / 2
+        # full_random = full_random.permute(1,2,0).contiguous()
+        # print("full random range: ", torch.min(full_random), torch.max(full_random))
+        # torch.save(half_random, res_dir + "/trail{}".format(TRAIL) + "/half_random_generates_epoch{}.pt".format(epoch))
+        # torch.save(full_random, res_dir + "/trail{}".format(TRAIL) + "/full_random_generates_epoch{}.pt".format(epoch))
+        # torch.save(transformer_model.state_dict(), res_dir + "/trail{}/pretrained_model/".format(TRAIL) + "epoch_{}.pt".format(epoch))
         
         del sample_data, rec_and_pred_img, orig, rec, half_random, full_random, full_random_mostlike
         
